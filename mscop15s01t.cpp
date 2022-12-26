802fb790d2e41b8c86430201d68734284ee0ef8e
H <106968861+Hgit-1@users.noreply.github.com>
2022-12-25 23:03:42 -0800

    Add files via upload

---
+++ mscop15s01t.cpp
@@ -0,0 +1,543 @@
+//(we do not show this page when you are using)
+//WARNING!THIS PROGRAM MAY HAVE ERROR!IF YOU FIND ERROR,PLEASE CALL H!e-mail:Caibohao0301@outlook.com/Caibohao0301@163.com/H20102020@outlook.com!
+//server load page
+//now all(7)server
+//1.0server:start,time:1day,upload day:unkhown/unkhown/unkhown/unkhown:unkhown:unkhown(this server was delete,if you want to find older vertion,please search wechat record)
+//1.1server:upload receive port(use new algorithm(before:check x[?](have error,classify on code,use if,ifelse,ifelseif),now:j-?,x[?](plus classify on code+length,use if,ifelse,ifelseif(less use)))),time:2hour,upload day:2022/6/3/21:50:00
+//1.2server:upload receive port(use new algorithm(1.1ser:use x[?],1.2ser:use a,b,c,d,e,f))time:2hour,upload day:2022/6/10/22:23:00(clear any error on normal use(DO NOT USE THIS UNNORMAL,OR IT WILL BE IN A FOREVER-LOOP OR SOMETHING ERROR!)
+//1.3server:upload send port(plus code to send to wechat or some thing web!)time:2hour,upload day:2022/6/12/15:46:00
+//1.4server:upload command unit(<>)time:10min,upload day:2022/7/29/18:33:00(now use on main)
+//1.5server:upload command unit,time:5min,upload day:2022/8/8/19:06:00(now use on main)
+//15s01t:upload send port(read CAP letters)upload day:2022/12/26
+#include<bits/stdc++.h>
+#include<stdlib.h>
+#include<windows.h>
+using namespace std;
+int mswechat() {
+	char x;
+	char CommandInsert1;
+	unsigned long long y;
+	cout << "how many words you want to cout?" << endl;
+	cin >> y;
+	cout << "WARNING!IF YOU WANT TO PRINT'......',YOU HAVE TO PRINT'|'!" << endl;
+	cout << "what to you want to compile?(includeing error(not in dictionary))" << endl;
+	for (int i = 1; i <= y; i++) {
+		cin >> x;//TODO
+		//cin>>CommandInsert1;
+		//x=CommandInsert1[0];
+		if (x == '<') {
+			//for (int j = 1; j <= 2; j++) {
+				//TODO
+				//cin >> CommandInsert1;
+				if (CommandInsert1 == 'c') {
+					system("cls");//TODO
+				} else if (CommandInsert1 == 'n') {
+					system("pause");//TODO
+				} else {
+					exit(3);
+				}
+			//	continue;
+			//}
+		}
+		else if (x == 'a'||x=='A') {
+			cout << ".-" << endl; //TODO
+		} else if (x == 'b'||x=='B') {
+			cout << "-..." << endl;
+		} else if (x == 'c'||x=='C') {
+			cout << "-.-." << endl;
+		} else if (x == 'd'||x=='D') {
+			cout << "-.." << endl;
+		} else if (x == 'e'||x=='E') {
+			cout << "." << endl;
+		} else if (x == 'f'||x=='F') {
+			cout << "..-." << endl;
+		} else if (x == 'g'||x=='G') {
+			cout << "--." << endl;
+		} else if (x == 'h'||x=='H') {
+			cout << "...." << endl;
+		} else if (x == 'i'||x=='I') {
+			cout << ".." << endl;
+		} else if (x == 'j'||x=='J') {
+			cout << ".---" << endl;
+		} else if (x == 'k'||x=='K') {
+			cout << "-.-" << endl;
+		} else if (x == 'l'||x=='L') {
+			cout << ".-.." << endl;
+		} else if (x == 'm'||x=='M') {
+			cout << "--" << endl;
+		} else if (x == 'n'||x=='N') {
+			cout << "-." << endl;
+		} else if (x == 'o'||x=='O') {
+			cout << "---" << endl;
+		} else if (x == 'p'||x=='P') {
+			cout << ".--." << endl;
+		} else if (x == 'q'||x=='Q') {
+			cout << "--.-" << endl;
+		} else if (x == 'r'||x=='R') {
+			cout << ".-." << endl;
+		} else if (x == 's'||x=='S') {
+			cout << "..." << endl;
+		} else if (x == 't'||x=='T') {
+			cout << "-" << endl;
+		} else if (x == 'u'||x=='U') {
+			cout << "..-" << endl;
+		} else if (x == 'v'||x=='V') {
+			cout << "...-" << endl;
+		} else if (x == 'w'||x=='W') {
+			cout << ".--" << endl;
+		} else if (x == 'x'||x=='X') {
+			cout << "-..-" << endl;
+		} else if (x == 'y'||x=='Y') {
+			cout << "-.--" << endl;
+		} else if (x == 'z'||x=='Z') {
+			cout << "--.." << endl;
+		} else if (x == '0') {
+			cout << "-----" << endl;
+		} else if (x == '1') {
+			cout << ".----" << endl;
+		} else if (x == '2') {
+			cout << "..---" << endl;
+		} else if (x == '3') {
+			cout << "...--" << endl;
+		} else if (x == '4') {
+			cout << "....-" << endl;
+		} else if (x == '5') {
+			cout << "....." << endl;
+		} else if (x == '6') {
+			cout << "-...." << endl;
+		} else if (x == '7') {
+			cout << "--..." << endl;
+		} else if (x == '8') {
+			cout << "---.." << endl;
+		} else if (x == '9') {
+			cout << "----." << endl;
+		} else if (x == '.') {
+			cout << ".-.-.-" << endl;
+		} else if (x == ',') {
+			cout << "--..--" << endl;
+		} else if (x == '!') {
+			cout << "---..." << endl;
+		} else if (x == '?') {
+			cout << "..--.." << endl;
+		} else if (x == '|') {
+			cout << ".----." << endl;
+		} else if (x == '-') {
+			cout << "-....-" << endl;
+		} else if (x == '(') {
+			cout << "-.--." << endl;
+		} else if (x == ')') {
+			cout << "-.--.-" << endl;
+		} else if (x == '/') {
+			cout << "-..-." << endl;
+		} else if (x == '=') {
+			cout << "-...-" << endl;
+		} else if (x == '+') {
+			cout << ".-.-." << endl;
+		} else if (x == '"') {
+			cout << ".-..-." << endl;
+		} else if (x == '*') {
+			cout << "-..-" << endl;
+		} else {
+			cout << "error" << endl;
+		}
+	}
+}
+int ms() {
+	char x, CommandInsert1;
+	unsigned long long y;
+	cout << "how many words you want to cout?" << endl;
+	cin >> y;
+	cout << "WARNING!IF YOU WANT TO PRINT'......',YOU HAVE TO PRINT'|'!" << endl;
+	cout << "what to you want to compile?(includeing error(not in dictionary))" << endl;
+	for (int i = 1; i <= y; i++) {
+		cin >> x;//TODO
+		if (x == '<') {
+			//for (int j = 1; j <= 2; j++) {
+				//TODO
+				//cin >> CommandInsert1;
+				if (CommandInsert1 == 'c') {
+					system("cls");//TODO
+				} else if (CommandInsert1 == 'n') {
+					system("pause");//TODO
+				} else {
+					exit(1);
+				}
+				//continue;
+			//}
+		}
+		if (x == 'a'||x=='A') {
+			cout << ".-" << " "; //TODO
+		} else if (x == 'b'||x=='B') {
+			cout << "-..." << " ";
+		} else if (x == 'c'||x=='C') {
+			cout << "-.-." << " ";
+		} else if (x == 'd'||x=='D') {
+			cout << "-.." << " ";
+		} else if (x == 'e'||x=='E') {
+			cout << "." << " ";
+		} else if (x == 'f'||x=='F') {
+			cout << "..-." << " ";
+		} else if (x == 'g'||x=='G') {
+			cout << "--." << " ";
+		} else if (x == 'h'||x=='H') {
+			cout << "...." << " ";
+		} else if (x == 'i'||x=='I') {
+			cout << ".." << " ";
+		} else if (x == 'j'||x=='J') {
+			cout << ".---" << " ";
+		} else if (x == 'k'||x=='K') {
+			cout << "-.-" << " ";
+		} else if (x == 'l'||x=='L') {
+			cout << ".-.." << " ";
+		} else if (x == 'm'||x=='M') {
+			cout << "--" << " ";
+		} else if (x == 'n'||x=='N') {
+			cout << "-." << " ";
+		} else if (x == 'o'||x=='O') {
+			cout << "---" << " ";
+		} else if (x == 'p'||x=='P') {
+			cout << ".--." << " ";
+		} else if (x == 'q'||x=='Q') {
+			cout << "--.-" << " ";
+		} else if (x == 'r'||x=='R') {
+			cout << ".-." << " ";
+		} else if (x == 's'||x=='S') {
+			cout << "..." << " ";
+		} else if (x == 't'||x=='T') {
+			cout << "-" << " ";
+		} else if (x == 'u'||x=='U') {
+			cout << "..-" << " ";
+		} else if (x == 'v'||x=='V') {
+			cout << "...-" << " ";
+		} else if (x == 'w'||x=='W'){
+			cout << ".--" << " ";
+		} else if (x == 'x'||x=='X') {
+			cout << "-..-" << " ";
+		} else if (x == 'y'||x=='Y') {
+			cout << "-.--" << " ";
+		} else if (x == 'z'||x=='Z') {
+			cout << "--.." << " ";
+		} else if (x == '0') {
+			cout << "-----" << " ";
+		} else if (x == '1') {
+			cout << ".----" << " ";
+		} else if (x == '2') {
+			cout << "..---" << " ";
+		} else if (x == '3') {
+			cout << "...--" << " ";
+		} else if (x == '4') {
+			cout << "....-" << " ";
+		} else if (x == '5') {
+			cout << "....." << " ";
+		} else if (x == '6') {
+			cout << "-...." << " ";
+		} else if (x == '7') {
+			cout << "--..." << " ";
+		} else if (x == '8') {
+			cout << "---.." << " ";
+		} else if (x == '9') {
+			cout << "----." << " ";
+		} else if (x == '.') {
+			cout << ".-.-.-" << " ";
+		} else if (x == ',') {
+			cout << "--..--" << " ";
+		} else if (x == '!') {
+			cout << "---..." << " ";
+		} else if (x == '?') {
+			cout << "..--.." << " ";
+		} else if (x == '|') {
+			cout << ".----." << " ";
+		} else if (x == '-') {
+			cout << "-....-" << " ";
+		} else if (x == '(') {
+			cout << "-.--." << " ";
+		} else if (x == ')') {
+			cout << "-.--.-" << " ";
+		} else if (x == '/') {
+			cout << "-..-." << " ";
+		} else if (x == '=') {
+			cout << "-...-" << " ";
+		} else if (x == '+') {
+			cout << ".-.-." << " ";
+		} else if (x == '"') {
+			cout << ".-..-." << " ";
+		} else if (x == '*') {
+			cout << "-..-" << " ";
+		} else {
+			cout << "error" << " ";
+		}
+	}
+}
+int mcout() {
+	char a, b, c, d, e, f;
+	int y;
+	int z;
+	cout << "how many codes you want to cout?" << endl;
+	cin >> y;
+	for (int i = 1; i <= y; i++) {
+		cout << "the length of the code?" << endl;
+		cin >> z;
+		if (z > 6) {
+			cout << "error" << endl; //TODO
+		}
+		if (z < 1) {
+			cout << "error" << endl; //TODO
+		}
+		cout << "WARNING!COMPILE MUST USE SPACE IMMEDIATELY!" << endl;
+		if (a == '<') {
+			//for (int j = 1; j <= 2; j++) {
+				//TODO
+				char CommandInsert1;
+				//cin >> CommandInsert1;
+				if (CommandInsert1 == 'c') {
+					system("cls");//TODO
+				} else if (CommandInsert1 == 'n') {
+					system("pause");//TODO
+				} else {
+					exit(1);
+				}
+				//continue;
+			//}
+		}
+		if (z == 1) {
+			cin >> a;
+			if (a == '.') {
+				cout << "E" << endl; //TODO
+			} else {
+				cout << "T" << endl; //TODO
+			}//TODO
+		} else if (z == 2) { //aimn
+			cin >> a >> b;
+			if (a == '.') { //ai
+				if (b == '-') {
+					cout << "A" << endl; //TODO
+				} else {
+					cout << "I" << endl; //TODO
+				}//TODO
+			} else { //mn
+				if (b == '-') {
+					cout << "M" << endl; //TODO
+				} else {
+					cout << "N" << endl; //TODO
+				}//TODO
+			}//TODO
+		}
+		if (z == 3) { //dgkorsuw
+			cin >> a >> b >> c;
+			if (a == '.') { //rsuw
+				if (b == '.') { //su
+					if (c == '.') {
+						cout << "S" << endl; //TODO
+					} else {
+						cout << "U" << endl; //TODO
+					}//TODO
+				} else { //rw
+					if (c == '.') {
+						cout << "R" << endl; //TODO
+					} else {
+						cout << "W" << endl; //TODO
+					}//TODO
+				}//TODO
+			} else { //dgko
+				if (b == '.') { //dk
+					if (c == '.') {
+						cout << "D" << endl; //TODO
+					} else {
+						cout << "K" << endl; //TODO
+					}//TODO
+				} else { //go
+					if (c == '.') {
+						cout << "G" << endl; //TODO
+					} else {
+						cout << "O" << endl; //TODO
+					}//TODO
+				}//TODO
+			}//TODO
+		} else if (z == 4) { //bcfhjlpqvxyz*
+			cin >> a >> b >> c >> d;
+			if (a == '.') { //jfhlpv
+				if (b == '.') { //fhv
+					if (c == '.') { //hv
+						if (d == '.') {
+							cout << "H" << endl; //TODO
+						} else {
+							cout << "V" << endl; //TODO
+						}//TODO
+					} else {
+						cout << "F" << endl; //TODO
+					}//TODO
+				} else { //jlp
+					if (c == '.') {
+						cout << "L" << endl; //TODO
+					} else {
+						if (d == '.') {
+							cout << "P" << endl; //TODO
+						} else {
+							cout << "J" << endl; //TODO
+						}//TODO
+					}//TODO
+				}//TODO
+			} else { //bcjqxyz*
+				if (b == '.') { //bcxy*
+					if (c == '.') { //bx*
+						if (d == '.') {
+							cout << "B" << endl; //TODO
+						} else {
+							cout << "X" << endl; //TODO
+						}//TODO
+					} else { //cy
+						if (d == '.') {
+							cout << "C" << endl; //TODO
+						} else {
+							cout << "Y" << endl; //TODO
+						}//TODO
+					}//TODO
+				} else { //qz
+					if (d == '.') {
+						cout << "Z" << endl; //TODO
+					} else {
+						cout << "Q" << endl; //TODO
+					}//TODO
+				}//TODO
+			}//TODO
+		} else if (z == 5) { //1234567890(/=+
+			cin >> a >> b >> c >> d >> e;
+			if (a == '.') { //12345+
+				if (b == '.') { //2345
+					if (c == '.') { //345
+						if (d == '.') { //45
+							if (e == '.') {
+								cout << 5 << endl; //TODO
+							} else {
+								cout << 4 << endl; //TODO
+							}//TODO
+						} else {
+							cout << 3 << endl; //TODO
+						}//TODO
+					} else {
+						cout << 2 << endl; //TODO
+					}//TODO
+				} else { //1+
+					if (c == '.') {
+						cout << "+" << endl;; //TODO
+					} else {
+						cout << 1 << endl; //TODO
+					}
+				}//TODO
+			} else { //67890(/=
+				if (b == '.') { //6(/=
+					if (c == '.') { //6/=
+						if (d == '.') { //6=
+							if (e == '.') {
+								cout << 6 << endl; //TODO
+							} else {
+								cout << "=" << endl; //TODO
+							}//TODO
+						} else {
+							cout << "/" << endl; //TODO
+						}//TODO
+					} else {
+						cout << "(" << endl; //TODO
+					}//TODO
+				} else { //7890
+					if (c == '.') {
+						cout << 7 << endl; //TODO
+					} else {
+						if (d == '.') {
+							cout << 8 << endl; //TODO
+						} else {
+							if (e == '.') {
+								cout << 9 << endl; //TODO
+							} else {
+								cout << 0 << endl; //TODO
+							}//TODO
+						}//TODO
+					}//TODO
+				}//TODO
+			}//TODO
+		} else if (z == 6) { //.,!?......-)"
+			cin >> a >> b >> c >> d >> e >> f;
+			if (a == '.') { //.?......"
+				if (b == '.') {
+					cout << "?" << endl; //TODO
+				} else { //."......
+					if (c == '.') { //."
+						if (d == '.') {
+							cout << '"' << endl;; //TODO
+						} else {
+							cout << "." << endl; //TODO
+						}//TODO
+					} else {
+						cout << "......" << endl; //TODO
+					}//TODO
+				}//TODO
+			} else { //,!-)
+				if (b == '.') {
+					if (c == '-') {
+						cout << ")" << endl; //TODO
+					} else {
+						cout << "-" << endl; //TODO
+					} //TODO
+				} else {
+					if (c == '.') {
+						cout << "," << endl; //TODO
+					} else {
+						cout << "!" << endl; //TODO
+					}//TODO
+				}//TODO
+			}//TODO
+		} else {
+			cout << "";
+		}
+	}
+}
+int main() {
+	int ask;
+	cout << "please input your ans(1-send,2-receive,3-wechat send,other-quit)" << endl;
+	cin >> ask;
+	if (ask == 1) {
+		ms();//TODO
+	} else if (ask == 2) {
+		mcout();//TODO
+	} else if (ask == 3) {
+		mswechat();
+	} else {
+		cout << "quit" << endl;
+	}
+	cout << "compile over";
+}
+//user manual(1.5ser)
+//1:what is this program?
+//	this is a program to send/compile morse-code.
+//2:use
+//2.1:first "cin"
+//		first "cin" is doing for ask which mode you want to use.
+//2.2:send
+//		we will ask you how many code you want to send(if you write too much,system may cout error/compile over too early).
+//		warning:this is a "unsigned-long-long mode",you can cin 0-18446744073709551615,
+//		DO NOT CIN NUMBER BIGGER THAN 18446744073709551615 OR SMALLER THAN 0,DO NOT CIN
+//		"float-mode(like:0.0,1.8......)",DO NOT CIN "char-mode(like:hello,?......)",
+//		OTHER USE "int-mode",DO NOT CIN NUMBER BIGGER THAN 2147483647,SMALLER THAN
+//		-2147483648!NITHER,YOU CAN NOT CIN "float-mode"OR"char-mode"!if it is char mode,
+//		you can cin char,number(DO NOT CIN NUMBER BIGGER THAN 9 AND SMALLER THAN 0!).
+//		you can cin some sentence and see/ctrl+c cout code!and wechat mode can plus enter!(we are so sorry that we did not record chinese)
+//2.3:receive
+//		we will ask you how many code you want to cin(if you write too much,system may cout error/compile over too early).
+//		"int mode" user manual is in "send".when you want to cin code,please cin like that:
+//		.-to. -or.+enter+-,we are checked if you cin on normal set there will be no problem!
+//2.4:quit
+//		if you cin a number apart from 1or2or3,we will end this program.
+//3:tips
+//	DO NOT CHANGE CODE IF YOU UNSAVE MAIN FILE OR YOU ARE NOT A PROGRAMMER!
+//	IF THERE ARE ANY ERROR,PLEASE CALL:"Caibohao0301@outlook.com"(send in:24h,send receive:14h)/"Caibohao0301@163.com"(send in:24h,send receive:unkhown)
+//	"H20102020@outlook.com"(send in:24h,send receive:14h)
+//	if you receive this flie,please test that keys are right.net:https://www.qsl.net/bg3tt/zl/mesdm.htm
+//4:command:<>:
+//	<clear>=clear+exit
+//	<normalstopsystem>=push any keys to continue+exit
+//	<emergencystopsystem>=stop system
+//5:flie creater:
+//	1:programmer:H
+//	2:ideas giver:H/Z
+//	3:support:H/Z/Red Panda Dev-C++ 6.5/source forge/qsl.net(bg3tt)
+//(we do not show this manual when you are using)
